dff6375fc197acd9ff822baab99e753a5f40ad1b
devitoDbug <davidOchiengy@gmail.com>
2023-10-26 18:11:17 +0300

    feat: Add Search UI and brand Name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
-  return <div className="h-full w-full bg-red-200"></div>;
+  return (
+    <div className="h-full w-full font-poppin flex flex-col">
+      <div className="h-[20%] px-8 pt-8">
+        <div className="text-2xl px-1 font-medium">
+          Text<span className="text-C_Blue ">Quill</span>
+        </div>
+        <div className="mt-4 flex justify-between border rounded-xl shadow-md p-3 text-sm text-C_TextWhiteDull border-C_WhiteGray">
+          <input
+            type="text"
+            placeholder="Search Document..."
+            className="bg-transparent w-3/4 border-none outline-none text-C_DullBlack"
+          />
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+      </div>
+
+      <div className="h-[70%]">body</div>
+
+      <div className="h-[10%]">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        C_Green: "#03A696",
-        C_GreenSlit: "#04c8b4",
-        C_DullWhite: "#fcfcfc",
-        C_White: "#FDFDFD",
+        C_Blue: "#3651DA",
+        C_Black: "#000",
+        C_DullBlack: "#60718B",
+        C_WhiteGray: "#D5DAE2",
         C_TextWhite: "#FDFDFE",
-        C_TextWhiteDull: "#F2F6F7",
-        C_TextBlack: "#52585D",
+        C_TextWhiteDull: "#747474",
+        C_GreyShades: "#FAFAFA",
         C_UserDullBlack: "#AFBCC680",
       },
-      animation :{
-        
+      animation: {},
+      theme: {},
+      fontFamily: {
+        poppin: ["Poppins", "sans"],
       },
-      theme: {
-      }
     },
   },
   plugins: [],
-}
-
+};
