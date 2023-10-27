43fefba5a593ffbcb021b13c2c73ba230adfa13a
devitoDbug <davidOchiengy@gmail.com>
2023-10-27 13:44:49 +0300

    style: Enhanced UI elements and aesthetics for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Home";
 import "./styles/navbar.scss";
 const App = () => {
   return (
-    <div className=" w-screen h-screen ">
+    <div className=" w-screen h-screen bg-C_GreyShades ">
       <Home />
     </div>
   );
