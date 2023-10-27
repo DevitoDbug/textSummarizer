63e5e216c73304ae88baab78bd8a0553c4f9d8a7
devitoDbug <davidOchiengy@gmail.com>
2023-10-27 10:26:55 +0300

    Refactored navigation icons: Improved organization and code reusability.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,35 +8,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Navbar = () => {
+  const navIcons = [
+    {
+      icon: faHome,
+      className: "stroke-C_DullBlack stroke-[30] text-white",
+    },
+    {
+      icon: faPlus,
+      className: "border-2  text-C_DullBlack rounded-md p-[0.13rem]",
+    },
+    {
+      icon: faBookmark,
+      className: "stroke-C_DullBlack stroke-[30] text-white",
+    },
+    {
+      icon: faFile,
+      className: "stroke-C_DullBlack stroke-[30] text-white",
+    },
+  ];
+
   return (
     <div className="px-6 py-4 flex gap-7 bg-C_GreyShades border-t-2 border-b-0  justify-around ">
-      <button>
-        <FontAwesomeIcon
-          icon={faHome}
-          className="stroke-C_DullBlack stroke-[30] text-white"
-        />
-      </button>
-
-      <button>
-        <FontAwesomeIcon
-          icon={faPlus}
-          className="border-2  text-C_DullBlack rounded-md p-[0.13rem]"
-        />
-      </button>
-
-      <button>
-        <FontAwesomeIcon
-          icon={faBookmark}
-          className="stroke-C_DullBlack stroke-[30] text-white"
-        />
-      </button>
-
-      <button>
-        <FontAwesomeIcon
-          icon={faFile}
-          className="stroke-C_DullBlack stroke-[30] text-white"
-        />
-      </button>
+      {navIcons.map((icon, index) => (
+        <button key={index}>
+          <FontAwesomeIcon icon={icon.icon} className={icon.className} />
+        </button>
+      ))}
     </div>
   );
 };
