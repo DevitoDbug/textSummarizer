6ce921d9d7ffdf1ad173cf8fd0c88461dc3b1332
devitoDbug <davidOchiengy@gmail.com>
2023-10-27 10:37:45 +0300

    feat: Added click animation for icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,30 +8,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Navbar = () => {
+  const [active, setActive] = React.useState(0);
   const navIcons = [
     {
       icon: faHome,
       className: "stroke-C_DullBlack stroke-[30] text-white",
+      move: "translate-y-[-1rem] duration-500",
     },
     {
       icon: faPlus,
       className: "border-2  text-C_DullBlack rounded-md p-[0.13rem]",
+      move: "translate-y-[-1rem] duration-500",
     },
     {
       icon: faBookmark,
       className: "stroke-C_DullBlack stroke-[30] text-white",
+      move: "translate-y-[-1rem] duration-500",
     },
     {
       icon: faFile,
       className: "stroke-C_DullBlack stroke-[30] text-white",
+      move: "translate-y-[-1rem] duration-500",
     },
   ];
 
   return (
     <div className="px-6 py-4 flex gap-7 bg-C_GreyShades border-t-2 border-b-0  justify-around ">
       {navIcons.map((icon, index) => (
-        <button key={index}>
-          <FontAwesomeIcon icon={icon.icon} className={icon.className} />
+        <button key={index} onClick={() => setActive(index)}>
+          <FontAwesomeIcon
+            icon={icon.icon}
+            className={`${icon.className} + ${
+              active === index ? icon.move : ""
+            }`}
+          />
         </button>
       ))}
     </div>
