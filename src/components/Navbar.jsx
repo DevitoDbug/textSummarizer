66534cf390d837e0af087893a93fc12efd16879f
devitoDbug <davidOchiengy@gmail.com>
2023-10-27 11:53:29 +0300

    style: Enhanced UI elements and aesthetics for the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,44 +5,69 @@ import {
   faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
-  const [active, setActive] = React.useState(0);
+  const [active, setActive] = useState(0);
   const navIcons = [
     {
       icon: faHome,
       className: "stroke-C_DullBlack stroke-[30] text-white",
       move: "translate-y-[-1rem] duration-500",
+      name: "Home",
     },
     {
       icon: faPlus,
-      className: "border-2  text-C_DullBlack rounded-md p-[0.13rem]",
+      className: " text-C_DullBlack stroke-[30] rounded-md ",
       move: "translate-y-[-1rem] duration-500",
+      name: "Add",
     },
     {
       icon: faBookmark,
       className: "stroke-C_DullBlack stroke-[30] text-white",
       move: "translate-y-[-1rem] duration-500",
+      name: "Bookmark",
     },
     {
       icon: faFile,
       className: "stroke-C_DullBlack stroke-[30] text-white",
       move: "translate-y-[-1rem] duration-500",
+      name: "Files",
     },
   ];
 
   return (
-    <div className="px-6 py-4 flex gap-7 bg-C_GreyShades border-t-2 border-b-0  justify-around ">
+    <div className="px-6  flex gap-7 bg-C_GreyShades border-t-2 border-b-0 justify-around overflow-hidden">
       {navIcons.map((icon, index) => (
-        <button key={index} onClick={() => setActive(index)}>
-          <FontAwesomeIcon
-            icon={icon.icon}
-            className={`${icon.className} + ${
-              active === index ? icon.move : ""
+        <div
+          className="w-14  h-14 flex flex-col relative  items-center justify-center"
+          key={index}
+        >
+          <button
+            onClick={() => setActive(index)}
+            className={`flex justify-center items-center ${icon.className} ${
+              active === index
+                ? icon.move
+                : "transform ease-in-out duration-500"
             }`}
-          />
-        </button>
+          >
+            <FontAwesomeIcon
+              icon={icon.icon}
+              className={`text-xs ${
+                active === index ? " text-C_Blue font-extrabold scale-150 " : ""
+              } transition-colors duration-300`}
+            />
+          </button>
+          <span
+            className={`text-xs absolute font-bold  text-C_Blue ${
+              active === index
+                ? "-bottom-6 opacity-100 transform ease-in-out -translate-y-8 duration-100"
+                : "-bottom-6 opacity-0 transform ease-in-out duration-100"
+            }`}
+          >
+            {icon.name}
+          </span>
+        </div>
       ))}
     </div>
   );
