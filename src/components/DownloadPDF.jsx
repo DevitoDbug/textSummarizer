c296f3044d306e233abea3d82abb4a47da7d8070
devitoDbug <davidOchiengy@gmail.com>
2023-11-10 20:31:43 +0300

    feat: add PDF download functionality

--- src/components/DownloadPDF.jsx
+++ src/components/DownloadPDF.jsx
@@ -5,10 +5,10 @@ import React from 'react';
 const DownloadPDF = () => {
   return (
     <>
-      <button className="flex items-center gap-1 rounded-3xl bg-C_Blue  pl-3  text-xs">
-        <span className="rounded-l-xl text-white">Download</span>
-        <span className="rounded-full bg-blue-600 p-1 text-white">
-          <span className="p-1 text-sm">
+      <button className="flex items-center gap-2 rounded-3xl border border-C_Blue pl-3 text-sm text-C_GreyBlue">
+        <span className="rounded-l-xl ">Download</span>
+        <span className="rounded-full border border-C_Blue bg-blue-600 p-1 text-white">
+          <span className="p-1 ">
             <FontAwesomeIcon icon={faArrowDown} />
           </span>
         </span>
